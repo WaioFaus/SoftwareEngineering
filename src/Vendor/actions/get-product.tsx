eec0bcfd4be2bebdd6ee2dea79f436c8e69b416e
sinful-johnny <125457000+sinful-johnny@users.noreply.github.com>
2023-12-27 19:43:05 +0700

    restructure src

--- src/Vendor/actions/get-product.tsx
+++
@@ -1,9 +0,0 @@
-import { Billboard, Product } from "@/type";
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
-
-const getProduct = async (id: string): Promise<Product> => {
-  const res = await fetch(`${URL}/${id}`);
-
-  return res.json();
-};
-export default getProduct;
--- src/Vendor/providers/toast-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Toaster } from "react-hot-toast";
-
-const ToastProdvider = () => {
-  return <Toaster />;
-};
-
-export default ToastProdvider;
