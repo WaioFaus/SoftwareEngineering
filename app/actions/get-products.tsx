3af0fa6194a392a2eaaa52cd38b910fe308d67ff
WaioFaus <129754983+WaioFaus@users.noreply.github.com>
2023-12-29 21:41:57 +0700

    buyer home page

---
+++ app/actions/get-products.tsx
@@ -0,0 +1,28 @@
+import { Product } from "@/type";
+import qs from "query-string";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+interface Query {
+  categoryId?: string;
+  colorId?: string;
+  sizeId?: string;
+  isFeatured?: boolean;
+  isArchived?: boolean;
+}
+
+const getProducts = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      colorId: query.colorId,
+      sizeId: query.sizeId,
+      categoryId: query.categoryId,
+      isFeatured: query.isFeatured,
+      isArchived: false,
+    },
+  });
+  const res = await fetch(url, { cache: "no-cache" });
+  return res.json();
+};
+export default getProducts;
