9c6467695259abb8e3b99512b3fd93ebd880771c
KietDev-JS <KietPersonalMail@gmail.com>
2023-12-30 18:46:38 +0700

    additional pages for newly addded apis
    pages with components and sub files commit

---
+++ src/admin/app/dashboard/(routes)/sizes/components/client.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { ApiList } from "@/components/ui/api-list";
+
+import { columns, SizeColumn } from "./columns";
+
+interface SizesClientProps {
+  data: SizeColumn[];
+}
+
+export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your products"
+        />
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+    </>
+  );
+};
