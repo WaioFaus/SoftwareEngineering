94f511280953d3ffcae4b6b5c1788237d62f10f8
WaioFaus <129754983+WaioFaus@users.noreply.github.com>
2024-01-02 18:05:45 +0700

    buyer hompage

---
+++ src/buyer/app/component/recentlyAdded-section.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+import ProductCard from "./ui/product-card";
+import { Product } from "@/type";
+
+interface products {
+    data: Product[];
+  }
+  
+  const RecentlyAddedSection: React.FC<products> = ({ data }) => {
+    const sortedData = data.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+  
+    const recentlyAddedProducts = sortedData.slice(0, 4);
+  
+    return (
+      <div className="flex-col mt-[140px] mx-[100px]">
+        <div className="flex justify-between  items-center pt-5">
+          <p className="text-3xl">Recently added</p>
+          <Button className="bg-[#DB4444] w-[159px] h-[56px] ">View All</Button>
+        </div>
+        <div className="flex justify-between  mt-[60px]">
+          {recentlyAddedProducts.map((item) => (
+            <ProductCard key={item.id} data={item} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default RecentlyAddedSection;
+  
